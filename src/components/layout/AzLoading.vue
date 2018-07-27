94b1d0da8c91a3b6f31e02ed46dddf202b297ba7
Adolfo Marangoni Hengling <adolfo.hengling@azi.com.br>
2018-07-27 16:22:32 -0400

    Componente AzLoading

---
+++ src/components/layout/AzLoading.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-layout row justify-center>
+        <v-dialog v-model="isLoading" persistent width="300">
+            <v-card color="white" class="black--text" dark>
+                <v-card-text>
+                    {{loadingMessage}}
+                    <v-progress-linear indeterminate color="primary" class="mb-0"></v-progress-linear>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </v-layout>
+</template>
+
+<script>
+    import {mapState} from 'vuex'
+    export default {
+        name: 'AzLoading',
+        computed: {
+            ...mapState('loki', ['isLoading', 'loadingMessage'])
+        }
+    }
+</script>
