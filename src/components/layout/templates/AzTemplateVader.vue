2283f79de919e5c1c5319e42b8b6e13bf5defdb4
ricardo.goncalves <rgoncalves@azi.com.br>
2018-11-26 10:04:55 -0300

    Ajustes

--- src/components/layout/templates/AzTemplateVader.vue
+++ src/components/layout/templates/AzTemplateVader.vue
@@ -1,7 +1,6 @@
 <template>
     <v-app class="az-template-vader" dark>
         <az-loading></az-loading>
-
         <az-aside>
             <div class="az-aside">
                 <div class="logo">
@@ -12,7 +11,6 @@
                 </div>
             </div>
         </az-aside>
-
         <v-toolbar app height="60" class="toolbar">
             <v-icon class="mobile_menu" @click="showAside()">dehaze</v-icon>
             <az-title/>
@@ -48,13 +46,15 @@
 </script>
 <style lang="stylus">
     .application--wrap
-        position: absolute !important
-        width: 100%
-        height: 100%
+        position absolute !important
+        width 100%
+        height 100%
     .v-content
-        height: 100%
+        height 100%
     .az-template-vader
         font-family 'Open Sans' !important
+        .az-avatar__logout
+            border-top 1px solid rgba(255,255,255,0.05) !important
         .az-about
             background-color var(--v-primary-lighten2) !important
             border-top 1px solid var(--v-primary-lighten2)
@@ -64,14 +64,13 @@
                 height 120px
                 color rgba(255,255,255,0.4) !important
             .az-menu
-
                 .v-list__tile__title
                     font-size 13px
                     color rgba(255,255,255,0.4)
-                    font-weight: normal
+                    font-weight normal
                 .v-list__tile
                     height 55px
-                    border-bottom: 1px solid rgba(255,255,255,0.05);
+                    border-bottom 1px solid rgba(255,255,255,0.05);
                     &:hover
                         .v-list__tile__title
                             color var(--v-secondary-lighten5) !important
@@ -83,11 +82,10 @@
                     .v-list__tile__title
                         font-weight bold
                         color var(--v-secondary-lighten5) !important
-
         .container
             background-color #2b2b2a
             height 100%
-            padding: 0
+            padding 0
         .toolbar
             box-shadow 0 2px 4px -1px rgba(0, 0, 0, .2)
             background-color var(--v-secondary-base) !important
@@ -96,32 +94,24 @@
                     color rgba(255,255,255,0.7) !important
                 &__subtitle
                     color rgba(255,255,255,0.7) !important
-
         .mobile_menu
             display none
-
     .application.theme--light
-        background: unset !important
-
+        background unset !important
     .application--wrap
-        min-height: unset !important
-
-
+        min-height unset !important
     html
-        overflow-y: auto
-
+        overflow-y auto
     @media (max-width: 720px)
         .az-template-default
             .mobile_menu
-                margin-right: 10px
-                display: unset !important
-                top: 1px
-                position: relative
-                color: var(--v-primary-base) !important
-
+                margin-right 10px
+                display unset !important
+                top 1px
+                position relative
+                color var(--v-primary-base) !important
             .az-title__subtitle
-                display: none
-
+                display none
             .no-mobile
-                display: none !important
+                display none !important
 </style>
