02d7e74babbe2746a8449f13ee20b5c00b71ef17
thiago.lodi <thiago.lodi@azi.com.br>
2020-01-20 13:38:31 -0400

    adicionando getters

---
+++ src/store/getters.js
@@ -0,0 +1,20 @@
+export default {
+    currentPageNum: state => {
+        return state.document.paginator.currentPageNum;
+    },
+    pageContainer: state => {
+        return {
+            height: state.document.pageContainer.height,
+            width: state.document.pageContainer.width
+        };
+    },
+    pages: state => {
+        return state.document.pages;
+    },
+    scale: state => {
+        return state.document.scale.current;
+    },
+    totalPageNum: state => {
+        return state.document.paginator.totalPageNum;
+    }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,6 @@ import mutations from './mutations'
 import mutationTypes from './mutation-types'
 import actions from './actions'
 import actionTypes from './action-types'
+import getters from './getters'
 
-export {state, mutations, actions, mutationTypes, actionTypes}
+export {state, mutations, actions, mutationTypes, actionTypes, getters}
