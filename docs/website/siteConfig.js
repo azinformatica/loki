807937c0bc3fffab9439daaf6889dc43a2cae297
Thalita <thalitaribeirooliveira@gmail.com>
2020-02-14 11:34:35 -0400

    Atualização do siteConfig.js

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -33,18 +33,10 @@ const users = [
 const siteConfig = {
   title: 'Loki', // Title for your website.
   tagline: 'Vue UI Components built with Vuetify framework',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
-  projectName: '@azinformatica/loki',
-  organizationName: '@azinformatica',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+  url: 'https://azinformatica.github.io',
+  baseUrl: '/loki/',
+  projectName: 'loki',
+  organizationName: 'azinformatica',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
