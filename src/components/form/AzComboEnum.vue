f516ad64c391574be3fe52b3735da313e55202f0
guilherme.campos <guilherme.campos@azi.com.br>
2020-02-18 15:31:47 -0400

    Inserido props selecione em AzComboEnum para inserir ou retirar objeto selecione no select e mudança no nome da props

--- src/components/form/AzComboEnum.vue
+++ src/components/form/AzComboEnum.vue
@@ -1,82 +1,82 @@
 <template>
     <v-select
-            :items="combo"
-            :value="value"
-            @input="alteraValor"
-            :label="label"
-            :placeholder="placeholder"
-            v-validate="{ required: isRequired }"
-            :name="name"
-            :error-messages="errors.collect(`${name}`)"
-            :disabled="disabled"
+        :items="combo"
+        :value="value"
+        @input="alteraValor"
+        :label="label"
+        :placeholder="placeholder"
+        v-validate="{ required: isRequired }"
+        :name="name"
+        :error-messages="errors.collect(`${name}`)"
+        :disabled="disabled"
     />
 </template>
 
 <script>
-    import _ from 'lodash'
+import _ from 'lodash'
 
-    export default {
-        props: {
-            enumObject: {
-                type: Object,
-                required: true
-            },
-            label: {
-                type: String,
-                default: ''
-            },
-            name: {
-                type: String,
-                default: ''
-            },
-            placeholder: {
-                type: String,
-                default: ''
-            },
-            isRequired: {
-                type: Boolean,
-                default: false
-            },
-            value: {
-                required: true
-            },
-            disabled: {
-                type: Boolean,
-                default: false
-            },
-            selecione:{
-                type: Boolean,
-                default: true
-            }
+export default {
+    props: {
+        enumObject: {
+            type: Object,
+            required: true
         },
-        inject: ['$validator'],
-        data() {
-            return {
-                combo: []
-            }
+        label: {
+            type: String,
+            default: ''
         },
-        beforeMount() {
-            this.combo = this.converteParaCombo(this.enumObject)
+        name: {
+            type: String,
+            default: ''
         },
-        methods: {
-            alteraValor(event) {
-                this.$emit('input', event)
-            },
-            converteParaCombo(objeto) {
-                let novoArray = []
+        placeholder: {
+            type: String,
+            default: ''
+        },
+        isRequired: {
+            type: Boolean,
+            default: false
+        },
+        value: {
+            required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        insertNullItem:{
+            type: Boolean,
+            default: true
+        }
+    },
+    inject: ['$validator'],
+    data() {
+        return {
+            combo: []
+        }
+    },
+    beforeMount() {
+        this.combo = this.converteParaCombo(this.enumObject)
+    },
+    methods: {
+        alteraValor(event) {
+            this.$emit('input', event)
+        },
+        converteParaCombo(objeto) {
+            let novoArray = []
 
-                if (objeto) {
-                    for (let [chave, valor] of Object.entries(objeto)) {
-                        novoArray.push({ text: valor, value: chave })
-                    }
-                    novoArray = _.sortBy(novoArray, 'text')
+            if (objeto) {
+                for (let [chave, valor] of Object.entries(objeto)) {
+                    novoArray.push({ text: valor, value: chave })
                 }
-                if(this.selecione){
-                    return _.union([{ text: 'Selecione', value: null }], novoArray)
-                }
-
-                return  novoArray
+                novoArray = _.sortBy(novoArray, 'text')
             }
+            if(this.insertNullItem){
+                return _.union([{ text: 'Selecione', value: null }], novoArray)
+            }
+
+            return  novoArray
         }
     }
+}
 </script>
