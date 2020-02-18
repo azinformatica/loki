8c999fe16baf7f0ca64d5495f8dfbaf4a811b8f0
guilherme.campos <guilherme.campos@azi.com.br>
2020-02-18 14:51:58 -0400

    Inserido props selecione em AzComboEnum para inserir ou retirar objeto selecione no select

--- src/components/form/AzComboEnum.vue
+++ src/components/form/AzComboEnum.vue
@@ -1,75 +1,82 @@
 <template>
     <v-select
-        :items="combo"
-        :value="value"
-        @input="alteraValor"
-        :label="label"
-        :placeholder="placeholder"
-        v-validate="{ required: isRequired }"
-        :name="name"
-        :error-messages="errors.collect(`${name}`)"
-        :disabled="disabled"
-    ></v-select>
+            :items="combo"
+            :value="value"
+            @input="alteraValor"
+            :label="label"
+            :placeholder="placeholder"
+            v-validate="{ required: isRequired }"
+            :name="name"
+            :error-messages="errors.collect(`${name}`)"
+            :disabled="disabled"
+    />
 </template>
 
 <script>
-import _ from 'lodash'
+    import _ from 'lodash'
 
-export default {
-    props: {
-        enumObject: {
-            type: Object,
-            required: true
-        },
-        label: {
-            type: String,
-            default: ''
-        },
-        name: {
-            type: String,
-            default: ''
-        },
-        placeholder: {
-            type: String,
-            default: ''
-        },
-        isRequired: {
-            type: Boolean,
-            default: false
-        },
-        value: {
-            required: true
-        },
-        disabled: {
-            type: Boolean,
-            default: false
-        }
-    },
-    inject: ['$validator'],
-    data() {
-        return {
-            combo: []
-        }
-    },
-    beforeMount() {
-        this.combo = this.converteParaCombo(this.enumObject)
-    },
-    methods: {
-        alteraValor(event) {
-            this.$emit('input', event)
-        },
-        converteParaCombo(objeto) {
-            let novoArray = []
-
-            if (objeto) {
-                for (let [chave, valor] of Object.entries(objeto)) {
-                    novoArray.push({ text: valor, value: chave })
-                }
-                novoArray = _.sortBy(novoArray, 'text')
+    export default {
+        props: {
+            enumObject: {
+                type: Object,
+                required: true
+            },
+            label: {
+                type: String,
+                default: ''
+            },
+            name: {
+                type: String,
+                default: ''
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            isRequired: {
+                type: Boolean,
+                default: false
+            },
+            value: {
+                required: true
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            selecione:{
+                type: Boolean,
+                default: true
             }
+        },
+        inject: ['$validator'],
+        data() {
+            return {
+                combo: []
+            }
+        },
+        beforeMount() {
+            this.combo = this.converteParaCombo(this.enumObject)
+        },
+        methods: {
+            alteraValor(event) {
+                this.$emit('input', event)
+            },
+            converteParaCombo(objeto) {
+                let novoArray = []
 
-            return _.union([{ text: 'Selecione', value: null }], novoArray)
+                if (objeto) {
+                    for (let [chave, valor] of Object.entries(objeto)) {
+                        novoArray.push({ text: valor, value: chave })
+                    }
+                    novoArray = _.sortBy(novoArray, 'text')
+                }
+                if(this.selecione){
+                    return _.union([{ text: 'Selecione', value: null }], novoArray)
+                }
+
+                return  novoArray
+            }
         }
     }
-}
 </script>
