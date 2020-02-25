6c5292cbaf3a17f108be5deb4261dcfa1a83b83f
thalita.oliveira <thalita.oliveira@azi.com.br>
2020-02-25 12:59:25 -0300

    - Update of files.

---
+++ docs/website/languages.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const languages = [
+  {
+    enabled: true,
+    name: 'English',
+    tag: 'en'
+  },
+  {
+    enabled: false,
+    name: 'Español',
+    tag: 'es-ES'
+  },
+  {
+    enabled: false,
+    name: 'Português (Brasil)',
+    tag: 'pt-BR'
+  }
+];
+module.exports = languages;
