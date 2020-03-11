7237d6f5a39985601bb92e61c6724b9f97696731
Matheus Machado <matheus.machado@azi.com.br>
2020-03-11 16:29:16 -0400

    Add option to show progress bar while fetching document
    Co-authored-by: Thalita Oliveira <thalitaribeirooliveira@gmail.com>

--- src/components/viewer/AzPdfDocumentViewer.vue
+++ src/components/viewer/AzPdfDocumentViewer.vue
@@ -1,19 +1,28 @@
 <template>
     <div class="az-pdf-document-viewer" justify-center :class="style">
-        <az-pdf-document-viewer-toolbar
-            v-bind="{ currentPage, totalPages }"
-            @zoomOut="resolveEventZoomOut"
-            @zoomIn="resolveEventZoomIn"
-            @resetZoom="resolveEventResetZoom"
+        <v-progress-linear
+                v-if="loading && progressBar"
+                v-model="progress"
+                :active="show"
+                :indeterminate="query"
+                :query="true"
         />
-        <div id="documentContainer" class="az-pdf-document-viewer__container" :style="{ height: height }">
-            <az-pdf-document-viewer-page
-                v-for="page in pages"
-                :key="page.pageIndex + 1"
-                :pageNum="page.pageIndex + 1"
-                :pageSize="pageSize"
-                @resize="resolveEventResize"
+        <div v-else>
+            <az-pdf-document-viewer-toolbar
+                    v-bind="{ currentPage, totalPages }"
+                    @zoomOut="resolveEventZoomOut"
+                    @zoomIn="resolveEventZoomIn"
+                    @resetZoom="resolveEventResetZoom"
             />
+            <div id="documentContainer" class="az-pdf-document-viewer__container" :style="{ height: height }">
+                <az-pdf-document-viewer-page
+                        v-for="page in pages"
+                        :key="page.pageIndex + 1"
+                        :pageNum="page.pageIndex + 1"
+                        :pageSize="pageSize"
+                        @resize="resolveEventResize"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -39,12 +48,21 @@ export default {
         height: {
             type: String,
             default: '100vh'
+        },
+        progressBar: {
+            type: Boolean,
+            default: false
         }
     },
     data: () => ({
         okToRender: false,
         pagesCanvasContext: {},
-        visiblePageNum: 1
+        visiblePageNum: 1,
+        progress: 0,
+        query: false,
+        show: true,
+        interval: 0,
+        loading: false
     }),
     computed: {
         computedSrc() {
@@ -78,6 +96,7 @@ export default {
         }
     },
     async mounted() {
+        this.queryAndIndeterminate()
         await this.getDocumentContainer().addEventListener('scroll', this.handleScroll)
         await this.updatePdfRendering()
     },
@@ -85,11 +104,14 @@ export default {
         async updatePdfRendering() {
             try {
                 this.$store.dispatch(actionTypes.DOCUMENT.CLEAR_RENDER_CONTEXT)
+                this.loading = true
                 await this.$store.dispatch(actionTypes.DOCUMENT.FETCH_DOCUMENT, this.computedSrc)
+                this.loading = false
             } catch (error) {
+                this.loading = false
                 if (this.computedSrc.length !== 0) {
                     this.$store.commit(mutationTypes.SHOW_ALERT, {
-                        message: 'URL do documento inválida.',
+                        message: 'URL do documento inválida..',
                         type: 'error'
                     })
                     throw new Error('URL do documento inválida.')
@@ -136,12 +158,33 @@ export default {
                 )
                 this.$store.dispatch(actionTypes.DOCUMENT.UPDATE_RENDERED_PAGES, payload.pageNum)
             }
+        },
+        queryAndIndeterminate () {
+            this.query = true
+            this.show = true
+            this.progress = 0
+
+            setTimeout(() => {
+                this.query = false
+
+                this.interval = setInterval(() => {
+                    if (this.progress === 100) {
+                        clearInterval(this.interval)
+                        this.show = false
+                        return setTimeout(this.queryAndIndeterminate, 2000)
+                    }
+                    this.progress += 25
+                }, 1000)
+            }, 2500)
         }
     },
     watch: {
         async computedSrc() {
             await this.updatePdfRendering()
         }
+    },
+    beforeDestroy () {
+        clearInterval(this.interval)
     }
 }
 </script>
