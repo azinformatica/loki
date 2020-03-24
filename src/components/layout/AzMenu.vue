1ef9b828cc05377f1f38c105ba1a98b79eeda62c
thszk <thsuzuqui@gmail.com>
2020-03-24 11:24:33 -0400

    fix the selected item detail on az-menu

--- src/components/layout/AzMenu.vue
+++ src/components/layout/AzMenu.vue
@@ -170,7 +170,7 @@ export default {
         border-top 8px solid transparent
         border-bottom 8px solid transparent
         border-right 10px solid #eee
-        left 95%
+        right 0
 
 .v-navigation-drawer--mini-variant .active-menu::after
     left 83%
