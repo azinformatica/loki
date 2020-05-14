5b118f709f93491e89b4adf72c2ec20e997d6836
thszk <thsuzuqui@gmail.com>
2020-05-14 11:35:55 -0400

    refactoring pdf viewer component

--- src/components/viewer/AzPdfDocumentViewer.vue
+++ src/components/viewer/AzPdfDocumentViewer.vue
@@ -1,43 +1,27 @@
 <template>
-    <v-flex xs12 class="az-pdf-document-viewer" justify-center :class="style">
-        <v-progress-linear
-            v-show="loading && progressBar"
-            v-model="progress"
-            :active="show"
-            :indeterminate="query"
-            :query="true"
+    <div class="az-pdf-container">
+        <Toolbar
+            :pagination="pagination"
+            @zoomIn="zoomIn"
+            @zoomOut="zoomOut"
+            @resetZoom="resetZoom"
+            v-show="!progressBar"
         />
-        <div v-show="!loading">
-            <az-pdf-document-viewer-toolbar
-                v-bind="{ currentPage, totalPages, downloadButton }"
-                @zoomOut="resolveEventZoomOut"
-                @zoomIn="resolveEventZoomIn"
-                @resetZoom="resolveEventResetZoom"
-                @download="createDownloadLink"
-            />
-            <div :id="`${id}-documentContainer`" class="az-pdf-document-viewer__container" :style="{ height: height }">
-                <az-pdf-document-viewer-page
-                    :id="id"
-                    v-for="page in pages"
-                    :key="page.pageIndex + 1"
-                    :pageNum="page.pageIndex + 1"
-                    :pageSize="pageSize[page.pageIndex]"
-                    @mounted="saveCanvasContext"
-                />
-            </div>
+        <div id="az-pdf-viewer" class="Viewer" :style="{ height: height }" v-show="!progressBar">
+            <div class="pdfViewer"></div>
         </div>
-    </v-flex>
+    </div>
 </template>
 
 <script>
-import AzPdfDocumentViewerToolbar from './AzPdfDocumentViewerToolbar'
-import AzPdfDocumentViewerPage from './AzPdfDocumentViewerPage'
-import { actionTypes } from '../../store'
-import scrollMonitor from 'scrollmonitor'
+import Toolbar from './Toolbar'
+import LoadingPlaceHolder from './LoadingPlaceHolder'
+import PDFJSLib from 'pdfjs-dist/build/pdf'
+import { PDFJS as PDFJSViewer } from 'pdfjs-dist/web/pdf_viewer.js'
+import 'pdfjs-dist/web/pdf_viewer.css'
 export default {
     components: {
-        AzPdfDocumentViewerToolbar,
-        AzPdfDocumentViewerPage
+        Toolbar
     },
     props: {
         id: {
@@ -54,7 +38,7 @@ export default {
         },
         height: {
             type: String,
-            default: '100vh'
+            default: 'calc(100vh - 64px)'
         },
         httpHeader: {
             type: Object,
@@ -70,209 +54,105 @@ export default {
         }
     },
     data: () => ({
-        documentContainerWidth: 0,
-        pagesCanvasContext: {},
-        pageWatcher: [],
-        progress: 0,
-        query: false,
-        show: true,
-        interval: 0,
-        loading: false
-    }),
-    computed: {
-        computedSrc() {
-            return this.src
+        pagination: {
+            current: null,
+            total: null
         },
-        currentPage() {
-            return this.$store.getters.currentPageNum
+        pdf: {
+            container: null,
+            eventBus: null,
+            viewer: null
         },
-        filename() {
-            return this.$store.getters.filename
-        },
-        pages() {
-            return this.$store.getters.pages
-        },
-        pageSize() {
-            return this.$store.getters.pageContainer
-        },
-        renderedPages() {
-            return this.$store.getters.renderedPages
-        },
-        style() {
-            let styleObj = {}
-            if (this.cssClass) {
-                const classes = this.cssClass.split(' ')
-                classes.forEach(clazz => (styleObj[clazz] = true))
-            }
-            return styleObj
-        },
-        totalPages() {
-            return this.$store.getters.totalPageNum
+        scale: {
+            default: null,
+            current: null
         }
-    },
+    }),
     mounted() {
-        this.queryAndIndeterminate()
+        this.start()
     },
     methods: {
-        async updatePdfRendering() {
-            try {
-                if (this.validatenNotEmptySrc()) {
-                    this.$store.dispatch(actionTypes.DOCUMENT.CLEAR_RENDER_CONTEXT)
-                    this.loading = true
-                    await this.$store.dispatch(actionTypes.DOCUMENT.FETCH_DOCUMENT, {
-                        src: this.computedSrc,
-                        httpHeader: this.httpHeader
-                    })
-                    this.loading = false
-                    this.setPageContainer()
-                    this.$store.dispatch(actionTypes.DOCUMENT.UPDATE_CURRENT_PAGE_NUM, 1)
-                    this.renderPage(1)
+        start() {
+            this.getPdfContainer()
+            this.createEventBus()
+            this.createPdfViewer()
+            this.renderDocument()
+        },
+        getPdfContainer() {
+            this.pdf.container = document.querySelector('#az-pdf-viewer')
+        },
+        createEventBus() {
+            const eventBus = new PDFJSViewer.EventBus()
+            eventBus.on('pagesinit', e => {
+                this.currentPage = e.source.currentPageNumber
+                this.totalPages = e.source.pagesCount
+                if (this.validateSmallScreen()) {
+                    this.pdf.viewer.currentScaleValue = 'page-width'
                 }
-            } catch (error) {
-                this.loading = false
-                if (this.validatenNotEmptySrc()) {
-                    if (error.message === 'Invalid PDF structure') {
-                        throw new Error('URL do documento inválida.')
-                    } else if (error.status === 401) {
-                        throw new Error('Não autorizado, verifique seu token de acesso.')
-                    }
-                }
-            }
+                this.scale.current = e.source.currentScale
+                this.scale.default = e.source.currentScale
+            })
+            eventBus.on('scalechange', e => {
+                this.scale.current = e.scale
+            })
+            eventBus.on('pagechange', e => {
+                this.currentPage = e.pageNumber
+            })
+            this.pdfEventBus = eventBus
         },
-        handleScroll() {
-            const containerMonitor = scrollMonitor.createContainer(this.getDocumentContainer())
-            for (let page of this.pages) {
-                this.pageWatcher[page.pageIndex] = containerMonitor.create(this.getDocumentPage(page.pageIndex + 1))
-                this.pageWatcher[page.pageIndex].fullyEnterViewport(() => {
-                    if (this.validatePageChange(page.pageIndex + 1)) {
-                        this.$store.dispatch(actionTypes.DOCUMENT.UPDATE_CURRENT_PAGE_NUM, page.pageIndex + 1)
-                    }
-                })
-                this.pageWatcher[page.pageIndex].enterViewport(async () => {
-                    if (this.needRenderCurrentPage()) {
-                        await this.renderPage(this.currentPage)
-                    }
-                    if (this.needRenderNextPage()) {
-                        await this.renderPage(this.currentPage + 1)
-                    }
-                    if (this.needRenderPreviousPage()) {
-                        await this.renderPage(this.currentPage - 1)
-                    }
-                })
-            }
+        createPdfViewer() {
+            this.pdf.viewer = new PDFJSViewer.PDFViewer({
+                container: this.pdf.container,
+                eventBus: this.pdfEventBus
+            })
         },
-        scrollToFirstPage() {
-            this.getDocumentContainer().scrollTop = 0
+        renderDocument() {
+            const loadingTask = PDFJSLib.getDocument({
+                url: this.src,
+                httpHeaders: this.httpHeaders,
+                withCredentials: true
+            })
+            loadingTask.promise.then(pdf => {
+                this.pdf.viewer.setDocument(pdf)
+            })
         },
-        resolveEventZoomOut() {
-            this.$store.dispatch(actionTypes.DOCUMENT.DECREASE_SCALE)
-            this.$store.dispatch(actionTypes.DOCUMENT.CLEAR_RENDERED_PAGES)
-            this.renderPage(this.currentPage)
+        validateSmallScreen() {
+            return this.pdf.container.clientWidth <= 700
         },
-        resolveEventZoomIn() {
-            this.$store.dispatch(actionTypes.DOCUMENT.INCREASE_SCALE)
-            this.$store.dispatch(actionTypes.DOCUMENT.CLEAR_RENDERED_PAGES)
-            this.renderPage(this.currentPage)
+        zoomIn() {
+            this.scale.current = this.scale.current * 1.1
+            this.pdf.viewer.currentScale = this.scale.current
         },
-        resolveEventResetZoom() {
-            this.$store.dispatch(actionTypes.DOCUMENT.CLEAR_RENDERED_PAGES)
-            this.setPageContainer()
-            this.renderPage(this.currentPage)
+        zoomOut() {
+            this.scale.current = this.scale.current / 1.1
+            this.pdf.viewer.currentScale = this.scale.current
         },
-        createDownloadLink() {
-            this.$store.dispatch(actionTypes.DOCUMENT.DOWNLOAD, { src: this.src, filename: this.filename })
-        },
-        async saveCanvasContext(payload) {
-            this.pagesCanvasContext[payload.pageNum] = payload
-        },
-        queryAndIndeterminate() {
-            this.query = true
-            this.show = true
-            this.progress = 0
-
-            setTimeout(() => {
-                this.query = false
-
-                this.interval = setInterval(() => {
-                    if (this.progress === 100) {
-                        clearInterval(this.interval)
-                        this.show = false
-                        return setTimeout(this.queryAndIndeterminate, 2000)
-                    }
-                    this.progress += 25
-                }, 1000)
-            }, 2500)
-        },
-        setPageContainer() {
-            if (this.documentContainerWidth <= 700) {
-                this.$store.dispatch(actionTypes.DOCUMENT.CALCULATE_SCALE, this.documentContainerWidth)
-            } else {
-                this.$store.dispatch(actionTypes.DOCUMENT.CALCULATE_SCALE)
-            }
-            this.$store.dispatch(actionTypes.DOCUMENT.UPDATE_PAGE_CONTAINER)
-        },
-        async renderPage(pageNum) {
-            this.$store.dispatch(actionTypes.DOCUMENT.UPDATE_RENDERED_PAGES, pageNum)
-            await this.$store.dispatch(actionTypes.DOCUMENT.RENDER_PAGE, this.pagesCanvasContext[pageNum])
-        },
-        getDocumentContainer() {
-            return document.getElementById(`${this.id}-documentContainer`)
-        },
-        getDocumentPage(pageNum) {
-            return document.getElementById(`${this.id}-page-${pageNum}`)
-        },
-        needRenderCurrentPage() {
-            return this.renderedPages.indexOf(this.currentPage) === -1
-        },
-        needRenderNextPage() {
-            return this.validateHasNextPage() && this.validateNextPageNotRendered()
-        },
-        validateHasNextPage() {
-            return this.currentPage < this.totalPages
-        },
-        validateNextPageNotRendered() {
-            return this.renderedPages.indexOf(this.currentPage + 1) === -1
-        },
-        needRenderPreviousPage() {
-            return this.validateHasPreviousPage() && this.validatePreviousPageNotRendered()
-        },
-        validateHasPreviousPage() {
-            return this.currentPage - 1 >= 1
-        },
-        validatePreviousPageNotRendered() {
-            return this.renderedPages.indexOf(this.currentPage - 1) === -1
-        },
-        validatePageChange(currentPage) {
-            return currentPage !== this.currentPage && currentPage <= this.totalPages
-        },
-        validatenNotEmptySrc() {
-            return this.computedSrc
+        resetZoom() {
+            this.pdf.viewer.currentScale = this.scale.default
         }
     },
     watch: {
         async computedSrc() {
-            this.documentContainerWidth = this.getDocumentContainer().clientWidth
-            await this.updatePdfRendering()
-            this.handleScroll()
-            this.scrollToFirstPage()
+            this.start()
         }
-    },
-    beforeDestroy() {
-        clearInterval(this.interval)
-        this.pageWatcher.forEach(watcher => {
-            watcher.destroy()
-        })
     }
 }
 </script>
 
-<style scoped lang="stylus">
-.az-pdf-document-viewer
-    position relative
-    width 100%
-    background-color #f8f9fa
-
-    &__container
-        height 100vh
+<style lang="stylus">
+.az-pdf-container
+    .Viewer
+        width 100%
+        position relative
         overflow scroll
+        background-color #e4e4e4
+
+        .page
+            margin 15px auto
+            border none
+            border-image none
+
+            .canvasWrapper
+                -webkit-box-shadow 0 3px 6px -1px rgba(0, 0, 0, .2)
+                box-shadow 0 3px 6px -1px rgba(0, 0, 0, .2)
 </style>
