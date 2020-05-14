40eff587328d1feed67fc97021ee5704cc0a8a38
thszk <thsuzuqui@gmail.com>
2020-05-14 11:40:16 -0400

    refactoring pdf viewer component - remove actions

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,18 +1,4 @@
 export default {
-    DOCUMENT: {
-        CALCULATE_SCALE: 'calculateScale',
-        CLEAR_RENDER_CONTEXT: 'clearRenderContex',
-        CLEAR_RENDERED_PAGES: 'clearRenderedPages',
-        DECREASE_SCALE: 'decreaseScale',
-        DOWNLOAD: 'download',
-        FETCH_DOCUMENT: 'fetchDocument',
-        INCREASE_SCALE: 'increaseScale',
-        RENDER_PAGE: 'renderPage',
-        RESTORE_SCALE: 'restoreScale',
-        UPDATE_CURRENT_PAGE_NUM: 'updateCurrentPageNum',
-        UPDATE_PAGE_CONTAINER: 'updatePageContainer',
-        UPDATE_RENDERED_PAGES: 'updateRenderedPages'
-    },
     SIGNATURE: {
         DIGITAL: {
             START: 'startDigitalSignature',
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import pdfjs from '../utils/AzPdfJsLib'
 import mutationTypes from './mutation-types'
 import actionTypes from './action-types'
 
@@ -33,97 +32,6 @@ export default {
         } catch (e) {
             commit(mutationTypes.SET_UPLOAD_FILE_PROGRESS_ERROR, hashName)
         }
-    },
-
-    async [actionTypes.DOCUMENT.DOWNLOAD](context, { src, filename }) {
-        await axios.get(src, { responseType: 'blob' }).then(response => {
-            const url = window.URL.createObjectURL(new Blob([response.data]))
-            const tempLink = document.createElement('a')
-            tempLink.href = url
-            tempLink.setAttribute('type', 'hidden')
-            tempLink.setAttribute('download', filename)
-
-            if (typeof tempLink.download === 'undefined') {
-                tempLink.setAttribute('target', '_blank')
-            }
-
-            document.body.appendChild(tempLink)
-            tempLink.click()
-
-            setTimeout(function() {
-                document.body.removeChild(tempLink)
-            }, 0)
-        })
-    },
-
-    async [actionTypes.DOCUMENT.FETCH_DOCUMENT](context, { src, httpHeader }) {
-        let pdf = await pdfjs.fetchDocument(src, httpHeader)
-        context.commit(mutationTypes.DOCUMENT.SET_TOTAL_PAGE_NUM, pdf.numPages)
-        context.commit(mutationTypes.DOCUMENT.SET_PAGES, await pdfjs.getPages(pdf))
-        context.commit(mutationTypes.DOCUMENT.SET_FILENAME, await pdfjs.getFileName(pdf))
-    },
-
-    [actionTypes.DOCUMENT.UPDATE_CURRENT_PAGE_NUM](context, currentPageNum) {
-        context.commit(mutationTypes.DOCUMENT.SET_CURRENT_PAGE_NUM, currentPageNum)
-    },
-
-    [actionTypes.DOCUMENT.UPDATE_PAGE_CONTAINER](context) {
-        let containerList = []
-        context.state.document.pages.forEach(page => {
-            let pageContainer = pdfjs.getPageContainer(page, context.state.document.scale.current)
-            containerList.push(pageContainer)
-        })
-        context.commit(mutationTypes.DOCUMENT.SET_PAGE_CONTAINER, containerList)
-    },
-
-    [actionTypes.DOCUMENT.CALCULATE_SCALE](context, containerWidth) {
-        if (containerWidth) {
-            let originalPageContainer = pdfjs.getPageContainer(
-                context.state.document.pages[0],
-                context.state.document.scale.default
-            )
-            let scale = containerWidth / originalPageContainer.width
-            context.commit(mutationTypes.DOCUMENT.SET_CURRENT_SCALE, scale)
-        } else {
-            context.commit(mutationTypes.DOCUMENT.SET_CURRENT_SCALE, context.state.document.scale.default)
-        }
-    },
-
-    [actionTypes.DOCUMENT.INCREASE_SCALE](context) {
-        if (context.state.document.scale.current < context.state.document.scale.max) {
-            context.commit(mutationTypes.DOCUMENT.SET_CURRENT_SCALE, context.state.document.scale.current + 0.25)
-            context.dispatch(actionTypes.DOCUMENT.UPDATE_PAGE_CONTAINER)
-        }
-    },
-
-    [actionTypes.DOCUMENT.DECREASE_SCALE](context) {
-        if (context.state.document.scale.current > context.state.document.scale.min) {
-            context.commit(mutationTypes.DOCUMENT.SET_CURRENT_SCALE, context.state.document.scale.current - 0.25)
-            context.dispatch(actionTypes.DOCUMENT.UPDATE_PAGE_CONTAINER)
-        }
-    },
-
-    async [actionTypes.DOCUMENT.RENDER_PAGE](context, { pageNum, canvasContext }) {
-        let page = context.state.document.pages[pageNum - 1]
-        let scale = context.state.document.scale.current
-        await pdfjs.renderPage({ page, scale, canvasContext })
-    },
-
-    [actionTypes.DOCUMENT.UPDATE_RENDERED_PAGES](context, pageNum) {
-        context.commit(mutationTypes.DOCUMENT.SET_RENDERED_PAGES, pageNum)
-    },
-
-    [actionTypes.DOCUMENT.CLEAR_RENDER_CONTEXT](context) {
-        context.commit(mutationTypes.DOCUMENT.SET_PAGES, [])
-        context.commit(mutationTypes.DOCUMENT.SET_RENDERED_PAGES, 'clear')
-        context.commit(mutationTypes.DOCUMENT.SET_TOTAL_PAGE_NUM, '-')
-        context.commit(mutationTypes.DOCUMENT.SET_CURRENT_PAGE_NUM, '-')
-        context.commit(mutationTypes.DOCUMENT.SET_CURRENT_SCALE, context.state.document.scale.default)
-        context.commit(mutationTypes.DOCUMENT.SET_PAGE_CONTAINER, [])
-    },
-
-    [actionTypes.DOCUMENT.CLEAR_RENDERED_PAGES](context) {
-        context.commit(mutationTypes.DOCUMENT.SET_RENDERED_PAGES, 'clear')
     },
 
     async [actionTypes.SIGNATURE.DIGITAL.START](context, { certificadoConteudo, documentId }) {
