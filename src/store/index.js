3f197dd25f893b4b1c0500489c757d9a8396dc41
thszk <thsuzuqui@gmail.com>
2020-05-28 00:34:43 -0400

    remove unused getters

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,5 @@ import mutations from './mutations'
 import mutationTypes from './mutation-types'
 import actions from './actions'
 import actionTypes from './action-types'
-import getters from './getters'
 
-export { state, mutations, actions, mutationTypes, actionTypes, getters }
+export { state, mutations, actions, mutationTypes, actionTypes }
