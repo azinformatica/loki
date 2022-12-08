72792c26d006371edfefe2ce0c7c96d527735e1b
Andre Luiz Santana Treu Afonso <andre.afonso@azi.com.br>
2022-12-08 09:11:32 -0400

    feat: criação AzDraggableTargetZoneItem

---
+++ src/components/draggable/AzDraggableTargetZoneItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div
+        :class="`${$parent.$options.propsData.draggableTargetZoneName} az-draggable-target-zone-item`"
+        :style="draggableTargetZonePosition"
+        :draggable-target-zone-item-id="draggableTargetZoneItemId"
+        @click="$emit('draggableTargetZoneItemClick', $event)"
+    >
+        <slot>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AzDraggableTargetZoneItem",
+    props: {
+        draggableTargetZoneItemId: {
+            type: String,
+            required: true
+        },
+        draggableTargetZoneItemRect: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        draggableTargetZonePosition() {
+            return {
+                left: `${this.draggableTargetZoneItemRect.x}px`,
+                top: `${this.draggableTargetZoneItemRect.y}px`,
+                width: `${this.draggableTargetZoneItemRect.width}px`,
+                height: `${this.draggableTargetZoneItemRect.height}px`
+            }
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+.az-draggable-target-zone-item
+    position absolute
+</style>
