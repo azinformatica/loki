cfd9b9cc35718559ce9082dca415ea133934f3b0
Andre Luiz Santana Treu Afonso <andre.afonso@azi.com.br>
2022-12-13 09:12:39 -0400

    fix: formataçao do codigo e melhorias AzDraggableItem

--- src/components/draggable/AzDraggableItem.vue
+++ src/components/draggable/AzDraggableItem.vue
@@ -1,39 +1,36 @@
 <template>
-    <div
-        :class="`${className} az-draggable-item`"
-        :id="id"
-    >
-        <slot>
-        </slot>
+    <div :class="`${className} az-draggable-item`" :id="id">
+        <slot> </slot>
     </div>
 </template>
 
 <script>
 export default {
-    name: "AzDraggableItem",
+    name: 'AzDraggableItem',
     props: {
         id: {
             type: String,
-            required: true
+            required: true,
         },
         targetZoneItemId: {
             type: String,
-            default: ''
+            default: '',
         },
         rect: {
             type: Object,
-            default: () => null
-        }
+            default: () => null,
+        },
     },
     data: () => ({
         mutationObserver: null,
         resizeObserver: null,
-        draggableTargetZoneItemElement: null
+        draggableTargetZoneItemElement: null,
     }),
     computed: {
         className() {
+            if (!this.$parent.$options.propsData) return ''
             return `${this.$parent.$options.propsData.name}-item`
-        }
+        },
     },
     mounted() {
         this.configureMutationObserver()
@@ -52,11 +49,11 @@ export default {
         },
         rect() {
             this.configureElementRect()
-        }
+        },
     },
     methods: {
         configureAttributeElementTargetZoneItemId() {
-            this.$el.setAttribute(`target-zone-item-id`, this.targetZoneItemId)
+            this.$el.setAttribute('target-zone-item-id', this.targetZoneItemId)
         },
         configureMutationObserver() {
             this.observeCurrentElementAttributesChanges()
@@ -77,12 +74,12 @@ export default {
         observeCurrentElementAttributesChanges() {
             this.getMutationObserver().observe(this.$el, {
                 attributes: true,
-                attributeFilter: ['data-x', 'data-y', 'data-width', 'data-height', 'target-zone-item-id']
+                attributeFilter: ['data-x', 'data-y', 'data-width', 'data-height', 'target-zone-item-id'],
             })
         },
         observeCurrentElementParentElementChildListChange() {
             this.getMutationObserver().observe(this.$parent.$el, {
-                childList: true
+                childList: true,
             })
         },
         mutationObserverCallback() {
@@ -126,7 +123,7 @@ export default {
             this.$el.setAttribute(rectAttributeName, rectAttributeValue)
         },
         getElementAttributeValueAsInt(rectAttributeName) {
-            return parseInt(this.$el.getAttribute(rectAttributeName) || 0)
+            return parseInt(this.$el.getAttribute(rectAttributeName)) || 0
         },
         updateElementStyleWidth() {
             const rectWidth = this.getElementAttributeValueAsInt('data-width')
@@ -146,7 +143,7 @@ export default {
             const destination = this.getDestinationPosition()
             const rect = {
                 x: this.getElementAttributeValueAsInt('data-x'),
-                y: this.getElementAttributeValueAsInt('data-y')
+                y: this.getElementAttributeValueAsInt('data-y'),
             }
 
             const translateX = destination.x - origin.x + rect.x
@@ -162,7 +159,7 @@ export default {
             this.$el.style.transform = transform
             return {
                 x: elementRect.x - parentElementRect.x,
-                y: elementRect.y - parentElementRect.y
+                y: elementRect.y - parentElementRect.y,
             }
         },
         getDestinationPosition() {
@@ -171,11 +168,11 @@ export default {
                 const draggableTargetZoneElementRect = this.draggableTargetZoneItemElement.getBoundingClientRect()
                 return {
                     x: draggableTargetZoneElementRect.x - parentElementRect.x,
-                    y: draggableTargetZoneElementRect.y - parentElementRect.y
+                    y: draggableTargetZoneElementRect.y - parentElementRect.y,
                 }
             }
             return { x: 0, y: 0 }
-        }
-    }
+        },
+    },
 }
 </script>
