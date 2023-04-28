f0078032fdffe2d2a25da16565cba956351f4578
andreLSTA <andre_lsta@hotmail.com>
2023-04-28 17:10:51 -0400

    feat: header timeline

--- src/components/bpm/AzBpmTimeline.vue
+++ src/components/bpm/AzBpmTimeline.vue
@@ -1,18 +1,27 @@
 <template>
     <v-timeline dense>
-        <v-timeline-item v-for="log in history" :key="log.taskName" :icon="log.icon">
+        <v-timeline-item color="primary" large>
+            <v-row class="mt-2">
+                <v-col> Data </v-col>
+                <v-col> Usuário </v-col>
+                <v-col> Atividade </v-col>
+                <v-col> Situação </v-col>
+                <v-col> Destino </v-col>
+            </v-row>
+        </v-timeline-item>
+        <v-timeline-item v-for="log in history" :key="log.date" :icon="log.icon">
             <v-row class="mt-2">
                 <v-col>
-                    {{ log.date }}
+                    {{ log.date | azDate('DD/MM/YYYY HH:mm:ss') }}
+                </v-col>
+                <v-col>
+                    {{ log.assignee }}
                 </v-col>
                 <v-col>
                     {{ log.taskName }}
                 </v-col>
                 <v-col>
                     {{ log.status }}
-                </v-col>
-                <v-col>
-                    {{ log.assignee }}
                 </v-col>
                 <v-col>
                     {{ log.nextTaskName }}
