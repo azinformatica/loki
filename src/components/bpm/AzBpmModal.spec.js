857afbbcc3681d6dffa8037f35000822ed82752d
andreLSTA <andre_lsta@hotmail.com>
2023-07-11 09:40:01 -0400

    feat: testes AzBpmModal

---
+++ src/components/bpm/AzBpmModal.spec.js
@@ -0,0 +1,78 @@
+import Vue from 'vue'
+import AzBpmModal from './AzBpmModal'
+import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+
+const localVue = createLocalVue()
+Vue.use(Vuetify)
+
+const createDefaultProps = () => {
+    return {
+        components: {
+            select: {
+                humanDecision: {
+                    show: true,
+                    disabled: false,
+                    items: [],
+                },
+            },
+            button: {
+                complete: {
+                    show: true,
+                    disabled: false,
+                    label: 'Encaminhar',
+                    action: jest.fn(),
+                },
+            },
+        },
+        buttonType: 'complete',
+        currentTask: {
+            name: 'Atividade inicial'
+        }
+    }
+}
+
+const createWrapper = ({ propsData = {}, shallow = true }) => {
+    const options = {
+        localVue,
+        propsData,
+        stubs: {
+            AzModal: true,
+        },
+        vuetify: new Vuetify(),
+    }
+    const mountingFunction = shallow ? shallowMount : mount
+    return mountingFunction(AzBpmModal, options)
+}
+
+describe('AzBpmModal.spec.js', () => {
+    let propsData, wrapper
+
+    beforeEach(() => {
+        propsData = createDefaultProps()
+        wrapper = createWrapper({ propsData, shallow: false })
+    })
+
+    describe('Props', () => {
+        it('Should receive components', () => {
+            expect(wrapper.props().components).toBe(propsData.components)
+        })
+
+        it('Should receive buttonType', () => {
+            expect(wrapper.props().buttonType).toBe(propsData.buttonType)
+        })
+
+        it('Should receive currentTask', () => {
+            expect(wrapper.props().currentTask).toBe(propsData.currentTask)
+        })
+    })
+
+    describe('Buttons', () => {
+        it('Should emit close event on press close icon', () => {
+            const buttonClose = wrapper.find('[data-test="button-close"]')
+            buttonClose.trigger('click')
+
+            expect(wrapper.emitted('close')).toBeTruthy()
+        })
+    })
+})
