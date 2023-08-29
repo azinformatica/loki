9da3bd0c0a1699ab9003cb373f07d52b7f67fdf1
andreLSTA <andre_lsta@hotmail.com>
2023-08-29 11:40:35 -0400

    feat: uos no AzBpmModal e melhoria na estilizaçao padrao

--- src/components/bpm/AzBpmModal.spec.js
+++ src/components/bpm/AzBpmModal.spec.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
+import 'regenerator-runtime/runtime'
 import AzBpmModal from './AzBpmModal'
 import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
+import Vue from 'vue'
 import Vuetify from 'vuetify'
+import Vuex from 'vuex'
 
 const localVue = createLocalVue()
+localVue.use(Vuex)
 Vue.use(Vuetify)
 
 const createDefaultProps = () => {
@@ -17,14 +20,32 @@ const createDefaultProps = () => {
                         {
                             value: 'value-ex-1',
                             text: 'text-ex-1',
+                            requiresUO: true,
                         },
                         {
                             value: 'value-ex-2',
                             text: 'text-ex-2',
+                            requiresUO: true,
                         },
                     ],
                 },
                 route: {
+                    show: false,
+                    disabled: false,
+                    items: [
+                        {
+                            value: 'value-ex-1',
+                            text: 'text-ex-1',
+                            requiresUO: true,
+                        },
+                        {
+                            value: 'value-ex-2',
+                            text: 'text-ex-2',
+                            requiresUO: true,
+                        },
+                    ],
+                },
+                uo: {
                     show: false,
                     disabled: false,
                     items: [
@@ -51,7 +72,11 @@ const createDefaultProps = () => {
         buttonType: 'complete',
         currentTask: {
             name: 'Atividade inicial',
+            currentUo: {
+                id: 'id-current-uo',
+            },
         },
+        show: true,
     }
 }
 
@@ -88,30 +113,34 @@ describe('AzBpmModal.spec.js', () => {
         it('Should receive currentTask', () => {
             expect(wrapper.props().currentTask).toBe(propsData.currentTask)
         })
+
+        it('Should receive show', () => {
+            expect(wrapper.props().show).toBe(propsData.show)
+        })
     })
 
-    describe('Buttons', () => {
+    describe('Events', () => {
         beforeEach(() => {
-            wrapper = createWrapper({ propsData, shallow: false })
+            wrapper = createWrapper({ propsData })
         })
 
         it('Should emit close event on press close icon', () => {
-            const buttonClose = wrapper.find('[data-test="button-close"]')
-            buttonClose.trigger('click')
+            const iconClose = wrapper.find('[data-test="icon-close"]')
+            iconClose.trigger('click')
 
             expect(wrapper.emitted('close')).toBeTruthy()
         })
 
-        it('Should emit close event on press cancel button', () => {
-            const buttonClose = wrapper.find('[data-test="button-cancel"]')
-            buttonClose.trigger('click')
+        it('Should emit close event on press cancel button', async () => {
+            const buttonCancel = wrapper.find('[data-test="button-cancel"]')
+            buttonCancel.vm.$emit('click')
 
             expect(wrapper.emitted('close')).toBeTruthy()
         })
 
         it('Should emit action event on press close icon', () => {
-            const buttonClose = wrapper.find('[data-test="button-action"]')
-            buttonClose.trigger('click')
+            const buttonAction = wrapper.find('[data-test="button-action"]')
+            buttonAction.vm.$emit('click')
 
             expect(wrapper.emitted('action')).toBeTruthy()
         })
@@ -150,11 +179,18 @@ describe('AzBpmModal.spec.js', () => {
                 expect(getSelect().exists()).toBe(true)
             })
 
+            it('Should disable human decision select if disabled', () => {
+                propsData.components.select.humanDecision.disabled = true
+                wrapper = createWrapper({ propsData })
+
+                expect(getSelect().vm.disabled).toBe(true)
+            })
+
             it('Should change selectedHumanTask when component emits change event', () => {
                 const selectedOption = propsData.components.select.humanDecision.items[1].value
                 getSelect().vm.$emit('input', selectedOption)
 
-                expect(wrapper.vm.selectedHumanTask).toBe(selectedOption)
+                expect(wrapper.vm.selectedHumanDecision).toBe(selectedOption)
             })
         })
 
@@ -166,23 +202,83 @@ describe('AzBpmModal.spec.js', () => {
                 wrapper = createWrapper({ propsData })
             })
 
-            it('Should not show human decision select if button type is not "route"', () => {
+            it('Should not show route select if button type is not "route"', () => {
                 propsData.buttonType = 'any-type-different-from-route'
                 wrapper = createWrapper({ propsData })
 
                 expect(getSelect().exists()).toBe(false)
             })
 
-            it('Should show human decision select if button type is "route"', () => {
+            it('Should show route select if button type is "route"', () => {
                 expect(getSelect().exists()).toBe(true)
             })
 
-            it('Should change selectedHumanTask when component emits change event', () => {
+            it('Should disable route select if disabled', () => {
+                propsData.components.select.route.disabled = true
+                wrapper = createWrapper({ propsData })
+
+                expect(getSelect().vm.disabled).toBe(true)
+            })
+
+            it('Should change selectedRoute when component emits change event', () => {
                 const selectedOption = propsData.components.select.route.items[1].value
                 getSelect().vm.$emit('input', selectedOption)
 
                 expect(wrapper.vm.selectedRoute).toBe(selectedOption)
             })
+        })
+
+        describe('UO', () => {
+            beforeEach(() => {
+                selector = '#uo-select'
+                propsData.components.select.uo.show = true
+                wrapper = createWrapper({ propsData })
+            })
+
+            it('Should not show uo select if not showed in components', () => {
+                propsData.components.select.uo.show = false
+                wrapper = createWrapper({ propsData })
+
+                expect(getSelect().exists()).toBe(false)
+            })
+
+            it('Should disable uo select if disabled', () => {
+                propsData.components.select.uo.disabled = true
+                wrapper = createWrapper({ propsData })
+
+                expect(getSelect().vm.disabled).toBe(true)
+            })
+
+            it('Should change selectedUO when component emits change event', () => {
+                const selectedOption = propsData.components.select.uo.items[1].value
+                getSelect().vm.$emit('input', selectedOption)
+
+                expect(wrapper.vm.selectedUO).toBe(selectedOption)
+            })
+        })
+    })
+
+    describe('Watch', () => {
+        beforeEach(() => {
+            const resetRequiresUO = (items) => {
+                items.forEach((item) => {
+                    item.requiresUO = false
+                })
+            }
+
+            resetRequiresUO(propsData.components.select.humanDecision.items)
+            resetRequiresUO(propsData.components.select.route.items)
+
+            wrapper = createWrapper({ propsData })
+        })
+
+        it('Should reset all selects on change "show"', async () => {
+            wrapper.vm.initializeAll = jest.fn()
+            await wrapper.setProps({ show: false })
+
+            expect(wrapper.vm.selectedUO).toBe('')
+            expect(wrapper.vm.selectedRoute).toBe(null)
+            expect(wrapper.vm.selectedHumanDecision).toBe(null)
         })
     })
 })
--- src/components/bpm/AzBpmModal.vue
+++ src/components/bpm/AzBpmModal.vue
@@ -1,60 +1,120 @@
 <template>
-    <az-modal>
-        <template #title>
-            <div class="d-flex flex-grow-1 flex-nowrap justify-space-between">
-                <h4>{{ actionLabel }}</h4>
-                <v-btn icon color="white" @click="emitCloseEvent" data-test="button-close">
-                    <v-icon> mdi-close </v-icon>
-                </v-btn>
-            </div>
-        </template>
-        <template #body>
-            <div class="pt-5">
-                <div class="d-flex mb-4">
-                    <div class="grey--text text--darken-3">Atividade atual:</div>
-                    <div class="ml-2 grey--text text--darken-1">{{ currentTaskName }}</div>
-                </div>
-                <div v-if="isButtonTypeComplete && components.select.humanDecision.show">
-                    <label for="human-decision-select" class="grey--text text--darken-3"> Encaminhar para: </label>
-                    <v-select
-                        id="human-decision-select"
-                        class="pt-0"
-                        placeholder="Selecione uma opção"
-                        v-model="selectedHumanTask"
-                        :items="components.select.humanDecision.items"
-                        hide-details
-                    ></v-select>
-                </div>
-                <div v-if="isButtonTypeRoute">
-                    <label for="route-select" class="grey--text text--darken-3"> Encaminhar para: </label>
-                    <v-select
-                        id="route-select"
-                        class="pt-0"
-                        placeholder="Selecione uma opção"
-                        v-model="selectedRoute"
-                        :items="components.select.route.items"
-                        hide-details
-                    >
-                    </v-select>
-                </div>
-            </div>
-        </template>
-        <template #actions>
-            <v-divider class="mx-2 pb-2"></v-divider>
-            <div class="d-flex flex-grow-1 justify-space-between pa-2">
-                <v-btn outlined color="grey lighten-1" @click="emitCloseEvent" data-test="button-cancel">
+    <v-dialog :value="show" scrollable persistent max-width="500px">
+        <v-card class="az-bpm-modal">
+            <slot name="title">
+                <v-card-title class="justify-center primary py-2 text-center text-subtitle-1 white--text">
+                    <span>{{ actionLabel }}</span>
+                    <v-spacer></v-spacer>
+                    <v-icon dark @click="$emit('close')" data-test="icon-close"> mdi-close</v-icon>
+                </v-card-title>
+            </slot>
+            <v-card-text class="pt-0 pb-0 pl-0 pr-0">
+                <v-row dense class="az-form-content">
+                    <v-col class="az-bpm-modal__item" cols="12">
+                        <div class="az-text">
+                            <label for="uo" class="grey--text text--darken-3">
+                                <b> Atividade Atual: </b>
+                            </label>
+                            <span class="ml-2 grey--text text--darken-1">
+                                {{ currentTaskName }}
+                            </span>
+                        </div>
+                    </v-col>
+                    <v-col class="az-bpm-modal__item" cols="12" v-if="isButtonTypeComplete && selectHumanDecisionShow">
+                        <div class="az-text">
+                            <label for="human-decision-select" class="grey--text text--darken-3">
+                                <b> Encaminhar para <span class="red--text">*</span> </b>
+                            </label>
+                        </div>
+                        <v-select
+                            id="human-decision-select"
+                            class="pt-0"
+                            placeholder="Selecione uma opção"
+                            dense
+                            v-model="selectedHumanDecision"
+                            :items="selectHumanDecisionItems"
+                            :disabled="selectHumanDecisionDisabled"
+                            return-object
+                            hide-details
+                        ></v-select>
+                    </v-col>
+                    <v-col class="az-bpm-modal__item" cols="12" v-if="isButtonTypeRoute && selectRouteShow">
+                        <div class="az-text">
+                            <label for="route-select" class="grey--text text--darken-3">
+                                <b> Encaminhar para <span class="red--text">*</span> </b>
+                            </label>
+                        </div>
+                        <v-select
+                            id="route-select"
+                            class="pt-0"
+                            placeholder="Selecione uma opção"
+                            dense
+                            v-model="selectedRoute"
+                            :items="selectRouteItems"
+                            :disabled="selectRouteDisabled"
+                            return-object
+                            hide-details
+                        >
+                        </v-select>
+                    </v-col>
+                    <v-col class="az-bpm-modal__item" cols="12" v-if="selectUOShow && selectedNextTaskRequiresUO">
+                        <div class="az-text">
+                            <label for="uo" class="grey--text text--darken-3">
+                                <b> Unidade Organizacional <span class="red--text">*</span> </b>
+                            </label>
+                        </div>
+                        <v-autocomplete
+                            id="uo-select"
+                            class="pt-0"
+                            dense
+                            placeholder="Selecione uma opção"
+                            v-model="selectedUO"
+                            :items="selectUOItems"
+                            :disabled="selectUODisabled"
+                            hide-details
+                            :menu-props="{ maxWidth: '468px' }"
+                        />
+                    </v-col>
+                </v-row>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn
+                    width="100px"
+                    class="text-capitalize"
+                    outlined
+                    color="grey lighten-1"
+                    @click="$emit('close')"
+                    data-test="button-cancel"
+                >
                     Cancelar
                 </v-btn>
-                <v-btn color="primary" @click="emitActionEvent" data-test="button-action"> {{ actionLabel }} </v-btn>
-            </div>
-        </template>
-    </az-modal>
+                <v-spacer></v-spacer>
+                <v-btn
+                    :disabled="selectedButton.disabled"
+                    width="100px"
+                    class="text-capitalize"
+                    color="primary"
+                    @click="emitActionEvent"
+                    data-test="button-action"
+                >
+                    {{ actionLabel }}
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
     name: 'AzBpmModal',
     props: {
+        show: {
+            type: Boolean,
+            default: false,
+        },
         buttonType: {
             type: String,
             default: '',
@@ -70,55 +130,111 @@ export default {
     },
     data() {
         return {
-            selectedHumanTask: '',
-            selectedRoute: '',
+            selectedUO: '',
+            selectedHumanDecision: null,
+            selectedRoute: null,
         }
     },
     methods: {
-        emitCloseEvent() {
-            this.$emit('close')
-        },
         emitActionEvent() {
-            const buttonType = this.buttonType
-            const selectedBpmParameters = {
-                humanDecision: this.selectedHumanTask,
-                activityIdDestination: this.selectedRoute,
-            }
-
-            this.$emit('action', { buttonType, selectedBpmParameters })
+            this.$emit('action', {
+                buttonType: this.buttonType,
+                bpmParameters: this.bpmParameters,
+            })
+        },
+        initializeUOSelect() {
+            this.selectedUO = this.originUOId || this.getFirstItemValue(this.selectUOItems)
+        },
+        resetUOSelect() {
+            this.selectedUO = ''
         },
         initializeHumanDecisionSelect() {
-            this.selectedHumanTask = this.getFirstItemValue(this.selectHumanDecisionItems)
+            if (this.isButtonTypeComplete) {
+                this.selectedHumanDecision = this.getFirstItem(this.selectHumanDecisionItems)
+            }
+        },
+        resetHumanDecisionSelect() {
+            this.selectedHumanDecision = null
         },
         initializeRouteSelect() {
-            this.selectedRoute = this.getFirstItemValue(this.selectRouteItems)
+            if (this.isButtonTypeRoute) {
+                this.selectedRoute = this.getFirstItem(this.selectRouteItems)
+            }
+        },
+        resetRouteSelect() {
+            this.selectedRoute = null
+        },
+        resetAll() {
+            this.resetUOSelect()
+            this.resetHumanDecisionSelect()
+            this.resetRouteSelect()
+        },
+        initializeAll() {
+            this.initializeUOSelect()
+            this.initializeHumanDecisionSelect()
+            this.initializeRouteSelect()
         },
         getFirstItemValue(items) {
-            const firstItem = items[0] || {}
+            const firstItem = this.getFirstItem(items)
 
-            return firstItem.value || ''
+            return firstItem ? firstItem.value : ''
+        },
+        getFirstItem(items) {
+            const [firstItem] = items
+
+            return firstItem || null
         },
     },
     watch: {
+        show() {
+            this.resetAll()
+            this.initializeAll()
+        },
         selectHumanDecisionItems() {
+            this.resetHumanDecisionSelect()
             this.initializeHumanDecisionSelect()
         },
         selectRouteItems() {
+            this.resetUOSelect()
             this.initializeRouteSelect()
+        },
+        selectUOItems() {
+            this.resetUOSelect()
+            this.initializeUOSelect()
+        },
+        originUO() {
+            this.resetUOSelect()
+            this.initializeUOSelect()
+        },
+        selectedNextTaskRequiresUO() {
+            this.resetUOSelect()
+            this.initializeUOSelect()
         },
     },
     computed: {
-        actionLabel() {
-            return this.selectedButton.label || '-'
+        nextTasks() {
+            return this.currentTask.nextTasks || []
         },
         currentTaskName() {
-            return (this.currentTask && this.currentTask.name) || '-'
+            return this.currentTask ? this.currentTask.name : '-'
+        },
+        actionLabel() {
+            return this.selectedButton.label || '-'
         },
         isButtonTypeComplete() {
             return this.buttonType === 'complete'
         },
         isButtonTypeRoute() {
             return this.buttonType === 'route'
+        },
+        selectedHumanDecisionRequiresUO() {
+            return this.selectedHumanDecision && this.selectedHumanDecision.requiresUO
+        },
+        selectedRouteRequiresUO() {
+            return this.selectedRoute && this.selectedRoute.requiresUO
+        },
+        selectedNextTaskRequiresUO() {
+            return this.selectedHumanDecisionRequiresUO || this.selectedRouteRequiresUO
         },
         selectedButton() {
             return this.components.button[this.buttonType] || {}
@@ -129,19 +245,100 @@ export default {
         selectHumanDecision() {
             return this.select.humanDecision || {}
         },
+        selectHumanDecisionShow() {
+            return this.selectHumanDecision.show || false
+        },
+        selectHumanDecisionDisabled() {
+            return this.selectHumanDecision.disabled || false
+        },
         selectHumanDecisionItems() {
             return this.selectHumanDecision.items || []
         },
         selectRoute() {
             return this.select.route || {}
         },
+        selectRouteShow() {
+            return this.selectRoute.show || false
+        },
+        selectRouteDisabled() {
+            return this.selectRoute.disabled || false
+        },
         selectRouteItems() {
             return this.selectRoute.items || []
         },
+        selectUO() {
+            return this.select.uo || {}
+        },
+        selectUOShow() {
+            return this.selectUO.show || false
+        },
+        selectUODisabled() {
+            return this.selectUO.disabled || false
+        },
+        selectUOItems() {
+            return this.selectUO.items
+        },
+        originUO() {
+            return this.currentTask ? this.currentTask.currentUo : null
+        },
+        originUOId() {
+            return this.originUO ? this.originUO.id.toString() : ''
+        },
+        bpmParameters() {
+            const selectedParameters = _.cloneDeep(this.uoParameters)
+
+            if (this.isButtonTypeComplete && this.selectedHumanDecision) {
+                _.merge(selectedParameters, this.completeParameters)
+            }
+
+            if (this.isButtonTypeRoute && this.selectedRoute) {
+                _.merge(selectedParameters, this.routeParameters)
+            }
+
+            return selectedParameters
+        },
+        completeParameters() {
+            return {
+                humanDecision: this.selectedHumanDecision && this.selectedHumanDecision.value,
+            }
+        },
+        routeParameters() {
+            return {
+                activityIdDestination: this.selectedRoute && this.selectedRoute.value,
+            }
+        },
+        uoParameters() {
+            return {
+                uoOriginId: this.originUOId,
+                uoDestinationId: this.selectedUO,
+            }
+        },
     },
     created() {
-        this.initializeHumanDecisionSelect()
-        this.initializeRouteSelect()
+        this.resetAll()
+        this.initializeAll()
     },
 }
 </script>
+<style lang="stylus">
+.az-bpm-modal
+    &__item
+        margin-bottom 16px
+
+        &:last-child
+            margin-bottom 0
+
+    .v-card
+        &__title
+            padding-left 16px !important
+            padding-right 16px !important
+
+        &__actions
+            padding 16px !important
+
+    .az-form-content
+        padding 16px !important
+
+        .col
+            padding 0 !important
+</style>
